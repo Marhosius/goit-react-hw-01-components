179fffbcdcbb8e3cbeda925d6c510ccc7da7a283
Marhosius <marhosius@gmail.com>
2023-05-04 01:26:30 +0300

    FriendList fix

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -3,14 +3,14 @@ import FriendListItem from "./FriendListItem";
 import css from './FriendList.module.css';
 
 const FriendList = ({ data }) => (
-    <ul className={css.friendList}>
-        <FriendListItem data={data} />
+    <ul className={css.friendList} key={css.friendList}>
+        {data.map((el) => < FriendListItem key={el.id} {...el} />)}
     </ul>
 )
 
 
 FriendList.propTypes = {
-    data: PropTypes.object,
+    data: PropTypes.array,
 };
 
 export default FriendList;
--- src/components/FriendList/FriendListItem.jsx
+++ src/components/FriendList/FriendListItem.jsx
@@ -2,20 +2,19 @@ import PropTypes from "prop-types";
 import css from './FriendList.module.css';
 import clsx from 'clsx';
 
-const FriendListItem = ({ data }) => data.map(({ avatar, name, isOnline, id }) => (
+const FriendListItem = ({ avatar, name, isOnline, id }) => (
     <li className={css.item} key={id}>
         <span className={clsx(isOnline ? css.online : css.offline)}></span>
         <img className={css.avatar} src={avatar} alt="User avatar" width="48" />
         <p className={css.name}>{name}</p>
     </li >
-))
-
+)
 
 FriendListItem.propTypes = {
-    id: PropTypes.number,
-    avatar: PropTypes.string,
-    name: PropTypes.string,
-    isOnline: PropTypes.bool,
+    id: PropTypes.number.isRequired,
+    avatar: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    isOnline: PropTypes.bool.isRequired,
 };
 
 export default FriendListItem;
